769d95ccf3007776b8ac2b7f0dbdc14bf54681b3
Sasha <Sasha@DELL>
2026-02-24 09:12:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Console.Write("Hello World!");
+            Console.ReadKey();
+            //undone means: work in progress
+            //to do means: to add a functionality   
+           /*hack means:temporarily solution*/
+
+}
+}
+}
